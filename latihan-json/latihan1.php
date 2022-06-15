161c67a73ab81ac5b107b6614315ad83c0919923
Hans Evan Tatipata <thansevan@gmail.com>
2022-06-16 01:40:40 +0700

    first commit

---
+++ latihan-json/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama" => "Hans",
+        "npm" => "12119740",
+        "email" => "thansevan@gmail.com"
+    ],
+    [
+        "nama" => "Hans",
+        "npm" => "12119740",
+        "email" => "thansevan@gmail.com"
+    ]
+];
+
+
+$data = json_encode($mahasiswa);
+echo $data;
+
+?>
